58f9d8fcdb892385f98ad90ffd483ac815394982
Rohan <rohanmisal89@gmail.com>
2024-03-06 11:32:08 +0530

    form completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import Forms from './components/Forms'
 
 function App() {
   return (
-   <div className='App'>
-    <h1>Hello</h1>
+   <div className='container'>
+    <Forms/>
    </div>
   )
 }
